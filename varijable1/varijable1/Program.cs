a633ffbe581f99cb7a20aca71cd1eb807ce9f83d
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-10 15:18:36 +0200

    Add files via upload

---
+++ varijable1/varijable1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varijable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5.0;
+            double b = 2.0;
+            double c = 4.3;
+            double d = 7.8;
+
+            double brojnik = Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2) + Math.Pow(d, 2);
+            double nazivnik = Math.Pow(a + b, 2) + Math.Pow(c + d,2);
+
+            double x = (brojnik / nazivnik);
+
+            Console.WriteLine($"Vrijednost izraza x je: {x}");
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
